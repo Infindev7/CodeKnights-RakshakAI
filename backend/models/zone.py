4c93f14e2885f4fce877663cc192d951738d5c21
Soham Pansare <pansaresoham10@gmail.com>
2026-01-27 21:53:58 +0530

    Created Database, Schemas, Models, and Routes. Updated all in the Main file.

---
+++ backend/models/zone.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Float, Enum
+from database import Base
+import enum
+
+class AreaType(str, enum.Enum):
+    RESIDENTIAL = "residential"
+    MARKET = "market"
+    HIGHWAY = "highway"
+
+class Zone(Base):
+    __tablename__ = "zones"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    zone_name = Column(String, unique=True, nullable=False, index=True)
+    center_lat = Column(Float, nullable=False)
+    center_lng = Column(Float, nullable=False)
+    radius = Column(Integer, nullable=False)  # in meters
+    area_type = Column(Enum(AreaType), nullable=False)
